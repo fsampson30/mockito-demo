f0f86dbeda9a28e130d5ecdb6a6b10f73d77407a
fsampson30 <fsampson30@gmail.com>
2024-03-01 07:38:13 -0300

    Added tests to mock a List.

--- src/test/java/com/sampson/mockito/mockitodemo/business/SomeBusinessImplTest.java
+++ src/test/java/com/sampson/mockito/mockitodemo/business/SomeBusinessImplTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -34,11 +37,34 @@ class SomeBusinessImplTest {
         assertEquals(25, business.findTheGreatestFromAllData());
     }
 
+
     @Test
     void findTheGreatestFromAllDataOneValueMock(){
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {25});
         assertEquals(25, business.findTheGreatestFromAllData());
     }
+
+    @Test
+    void listMockingTest(){
+        List listMock = mock(List.class);
+        when(listMock.size()).thenReturn(3);
+        assertEquals(3,listMock.size());
+    }
+
+    @Test
+    void listMockingParametersTest(){
+        List listMock = mock(List.class);
+        when(listMock.get(0)).thenReturn("SomeString");
+        assertEquals("SomeString",listMock.get(0));
+    }
+
+    @Test
+    void listMockingGenericParametersTest(){
+        List listMock = mock(List.class);
+        when(listMock.get(Mockito.anyInt())).thenReturn("SomeOtherString");
+        assertEquals("SomeOtherString",listMock.get(99));
+    }
+
 }
 
 class DataServiceStub implements DataService{
