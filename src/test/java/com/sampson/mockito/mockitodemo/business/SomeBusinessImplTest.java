ecf617847132a668752ab5d908a417fefe6a63d8
fsampson30 <fsampson30@gmail.com>
2024-03-01 07:18:48 -0300

    Created unit test using mock.

--- src/test/java/com/sampson/mockito/mockitodemo/business/SomeBusinessImplTest.java
+++ src/test/java/com/sampson/mockito/mockitodemo/business/SomeBusinessImplTest.java
@@ -3,6 +3,8 @@ package com.sampson.mockito.mockitodemo.business;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class SomeBusinessImplTest {
 
@@ -12,6 +14,15 @@ class SomeBusinessImplTest {
         SomeBusinessImpl business = new SomeBusinessImpl(dataServiceStub);
         int result = business.findTheGreatestFromAllData();
         assertEquals(25,result);
+    }
+
+    @Test
+    void findTheGreatestFromAllDataMock(){
+        DataService dataServiceMock = mock(DataService.class);
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {25,15,3});
+        SomeBusinessImpl business = new SomeBusinessImpl(dataServiceMock);
+        assertEquals(25, business.findTheGreatestFromAllData());
+
     }
 }
 
